94c3a944a04e9ab9931aa5a1a406e38d4263a1e4
unknown <2426355629@qq.com>
2020-05-24 23:13:04 +0800

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { FormGroup, AbstractControl, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 
+function userNameValidator(control: FormControl): { [s: string]: boolean } {
+  if (!control.value.match(/^a/)) {
+    return { invalidUser: true };
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'login-app';
+  // 对应我们登录的表单
+  myForm: FormGroup;
+  // 输入用户名的输入控件
+  userName: AbstractControl;
+  // 输入密码的输入控件
+  password: AbstractControl;
+
+  name$: Observable<string>;
+  constructor(private fb: FormBuilder) {
+    // 创建表单
+    this.myForm = this.fb.group(
+      {
+        'userName': ['aaa', Validators.compose([Validators.required, userNameValidator])],
+        'password': ['', Validators.compose([Validators.required, Validators.minLength(5)])]
+      });
+    // 关联 
+    this.userName = this.myForm.controls['userName'];
+    this.password = this.myForm.controls['password'];
+    this.name$ = this.userName.valueChanges;
+    this.userName.valueChanges.subscribe(val => {
+      console.log(val);
+    });
+  }
+  onSubmit(value: any) {
+    console.log(value);
+  }
 }
